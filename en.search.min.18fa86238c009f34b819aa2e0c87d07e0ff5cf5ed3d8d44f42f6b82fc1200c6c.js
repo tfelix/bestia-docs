9e258d060865c2c55f9160ec0475f729bbd93605
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-12 01:29:37 +0000

    Deploy tfelix/bestia-docs to github.com/tfelix/bestia-docs.git:gh-pages

--- en.search.min.18fa86238c009f34b819aa2e0c87d07e0ff5cf5ed3d8d44f42f6b82fc1200c6c.js
+++ en.search.min.18fa86238c009f34b819aa2e0c87d07e0ff5cf5ed3d8d44f42f6b82fc1200c6c.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.0488e0fe09c9357410185883ddb6a0d23b2865f476a1f9ff71ad33188fc2e8d4.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.b9c1f6d14c82ee97dbb86ac524cfc7d570d120bb511a5ab5d87c8ff58f9eba1d.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
